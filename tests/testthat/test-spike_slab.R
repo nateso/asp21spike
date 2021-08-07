c560f5fa08ad1a4d64405dd487c0e55f0d751d67
Jan Schick <jan.schick@stud.uni-goettingen.de>
2021-08-07 18:50:35 +0200

    test for derivatives added

--- tests/testthat/test-spike_slab.R
+++ tests/testthat/test-spike_slab.R
@@ -3,26 +3,29 @@ library(MASS)
 
 set.seed(3487)
 n <- 100
-mu_S = c(2,4,6,8,10)
+mu_S = rep(0, 5) # c(2,4,6,8,10)
 sigma_S = diag(1,nrow = 5,ncol = 5)
 S = mvrnorm(n, mu = mu_S, Sigma = sigma_S)
 
 bets <- matrix(c(-2,6,3,0,0))
 gams <- matrix(c(0,0,5,1,-10))
 
-y <- rnorm(n, S %*% bets, exp(S %*% gams))
+y <- S %*% bets
+y <- c(y + rnorm(100, 0, c(exp(S %*% gams))))
+# y <- rnorm(n, S %*% bets, exp(S %*% gams))
 
 
 
 test_data <- cbind.data.frame(y,S)
 names(test_data) <- c("y",paste0("s",1:5))
-
+str(test_data)
 
 m <- lmls(y ~ s1 + s2 + s3 + s4,
           scale = ~ s1 + s2 + s3 + s4 ,
           data = test_data,
           light =FALSE)
 
+m
 
 
 v0 <- 0.01
