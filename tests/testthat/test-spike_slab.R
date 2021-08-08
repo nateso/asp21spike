a7a304f95e6dda471b00a52f97f605194a636e0b
Nathanael Schmidt-Ott <n.schmidt.ott@gmail.com>
2021-08-08 15:59:42 +0200

    add unit tests

--- tests/testthat/test-spike_slab.R
+++ tests/testthat/test-spike_slab.R
@@ -1,57 +1,99 @@
 
-library(MASS)
-
-set.seed(1337)
+# simulate some data -----------------------------------------------
+set.seed(1509)
 n <- 100
 
-mu_S = rep(0, 5) # c(2,4,6,8,10)
-sigma_S = diag(1,nrow = 5,ncol = 5)
-S = mvrnorm(n, mu = mu_S, Sigma = sigma_S)
+mu <- c(-4,2,0,2,4)
+sigma <- rep(1,length(mu))
+
+X <- matrix(NA,nrow = n, ncol = length(mu))
+for(jj in 1:length(mu)){
+  X[,jj] <- rnorm(n,mu[jj],sigma[jj])
+}
 
 bets <- matrix(c(-2,6,3,0,0))
-gams <- matrix(c(0,0,5,1,-10))
+gams <- matrix(c(0,0,5,1,-1))
 
-y <- rnorm(n, S %*% bets, exp(-3 + S %*% gams))
+y <- rnorm(n, X %*% bets, exp(-3 + X %*% gams))
+y <- rnorm(n,0,4)
 
+test_data <- cbind.data.frame(y,X)
+names(test_data) <- c("y",paste0("x",1:5))
 
-
-
-test_data <- cbind.data.frame(y,S)
-names(test_data) <- c("y",paste0("s",1:5))
-str(test_data)
-
-m <- lmls(y ~ s1 + s2 + s3 + s4,
-          scale = ~ s1 + s3 + s4 ,
+m <- lmls(y ~ x1 + x2 + x3 + x4 + x5,
+          scale = ~ x1 + x2 + x3 + x4 + x5,
           data = test_data,
-          light =TRUE)
+          light = FALSE,maxit = 1000)
 
-m
-
-
-v0 <- 0.01
-M <- 10000
-stepsize <- .01
+# test spike_slab -------------------------------------------
+spsl <- spike_slab(m,v_0 = 0.000001)
+barplot(colMeans(spsl$spike$delta$location))
 
 
+set.seed(1312424)
+n <- 200
+snr <- 3
+sm1 <- runif(n)
+fsm1 <- dbeta(sm1, 7, 3)/2
+sm2 <- runif(n, 0, 1)
+f <- gl(3, n/3)
+ff <- as.numeric(f)/2
+fsm2f <- ff + ff * sm2 + ((f == 1) * -dbeta(sm2, 6, 4) +
+                               + (f == 2) * dbeta(sm2, 6, 9) + (f == 3) * dbeta(sm2,
+                                                                                + 9, 6))/2
+lin <- matrix(rnorm(n * 3), n, 3)
+colnames(lin) <- paste("lin", 1:3, sep = "")
+noise1 <- sm1 + rnorm(n)
+noise2 <- runif(n)
+noise3 <- runif(n)
+noise4 <- sample(gl(4, n/4))
+eta <- fsm1 + fsm2f + lin %*% c(0.1, 0.2, 0.3)
+y <- eta + sd(eta)/snr * rt(n, df = 5)
+d <- data.frame(y, sm1, sm2, f, lin, noise1, noise2,
+                   noise3, noise4)
+f1 <- y ~ (sm1 + sm2 + f + lin1)^2 + lin2 + lin3 + noise1 + 
+  noise2 + noise3 + noise4
 
-n <- 10000
-x1 <- runif(n)
-x2 <- runif(n)
-x3 <- runif(n)
-x4 <- runif(n)
-x5 <- runif(n)
-y <- rnorm(n, 0 + 1 * x1 + 2 * x3 - 6*x4, exp(-3 + 1 * x2 + 1 * x3))
-test_data <- cbind.data.frame(y,x1,x2,x3,x4,x5)
-
-m <- lmls(y ~ x1 + x2 + x3 + x4 + x5, ~ x2 + x3 + x4 + x5,
-          data = test_data, light = FALSE)
+m <- lmls(location = f1,scale = f1,
+     data = d,
+     light = FALSE,maxit = 1000)
 
 res <- spike_slab(m)
+colMeans(res$spike$delta$location)
 
 
+# check helper functions for update
+delta <- rep(c(0,1),2)
+bets <- c(2,3,-3,5)
+a_tau <- 1 
+b_tau <- 1
+a_theta <- 1
+b_theta <- 1
+v_0 <- 0.0000001
+theta <- 0.5
 
+tau <- sample_tau(delta, a_tau, b_tau, v_0)
 
+test_that('sample_tau() gives a vector of correct length',{
+  expect_true(length(tau) == length(delta))
+})
 
+test_that('sample_tau() gives small values if delta = 0 and large if delta = 1',{
+  expect_true(all(tau[delta == 0] < 0.1))
+  expect_true(all(tau[delta == 1] > 0.1))
+})
 
+delta_update <- update_delta(theta,tau,a_tau,b_tau,v_0)
+test_that("update_delta() yields one or zeros",{
+  expect_true(all(delta_update %in% c(0,1)))
+})
+
+theta_update <- update_theta(delta,a_theta, b_theta)
+test_that("update_theta() yields one value between 0 and 1",{
+  expect_true(all(theta_update >= 0,theta_update <= 1))
+  expect_true(length(theta_update) == 1)
+})
+
+tau_update <- update_tau(delta,bets,a_theta,b_theta,v_0)
 
 
