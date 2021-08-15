e04ac3023efef217decfe09465f5d3c11488cad6
Jan Schick <jan.schick@stud.uni-goettingen.de>
2021-08-15 12:23:44 +0200

    mmala sampler equipped with tau argument

--- R/mala_spike.R
+++ R/mala_spike.R
@@ -1,9 +1,9 @@
 #' @importFrom stats coef
 
-mmala_propose_spike <- function(curr_m, predictor, stepsize) {
+mmala_propose_spike <- function(curr_m, curr_tau, predictor, stepsize) {
   curr_coef <- coef(curr_m, predictor)
-  curr_score <- score_spike(curr_m, predictor)
-  curr_chol_info <- chol_info_spike(curr_m, predictor)
+  curr_score <- score_spike(curr_m, curr_tau, predictor)
+  curr_chol_info <- chol_info_spike(curr_m, curr_tau, predictor)
   
   step <- backsolve(
     r = curr_chol_info,
@@ -24,11 +24,11 @@ mmala_propose_spike <- function(curr_m, predictor, stepsize) {
 
 #' @importFrom stats coef
 
-mmala_backward_spike <- function(curr_m, prop_m, predictor, stepsize) {
+mmala_backward_spike <- function(curr_m, prop_m, curr_tau, predictor, stepsize) {
   curr_coef <- coef(curr_m, predictor)
   prop_coef <- coef(prop_m, predictor)
-  prop_score <- score_spike(prop_m, predictor)
-  prop_chol_info <- chol_info_spike(prop_m, predictor)
+  prop_score <- score_spike(prop_m, curr_tau, predictor)
+  prop_chol_info <- chol_info_spike(prop_m, curr_tau, predictor)
   
   step <- backsolve(
     r = prop_chol_info,
@@ -47,16 +47,16 @@ mmala_backward_spike <- function(curr_m, prop_m, predictor, stepsize) {
 
 #' @importFrom stats logLik runif
 
-mmala_update_spike <- function(curr_m, predictor, stepsize) {
-  proposal <- mmala_propose_spike(curr_m, predictor, stepsize)
+mmala_update_spike <- function(curr_m, curr_tau, predictor, stepsize) {
+  proposal <- mmala_propose_spike(curr_m, curr_tau, predictor, stepsize)
   prop_coef <- proposal$prop_coef
   forward <- proposal$forward
   
   prop_m <- set_coef(curr_m, predictor, prop_coef)
-  backward <- mmala_backward_spike(curr_m, prop_m, predictor, stepsize)
+  backward <- mmala_backward_spike(curr_m, prop_m, curr_tau, predictor, stepsize)
   
   curr_coef <- coef(curr_m,predictor)
-  Tau <- diag(curr_m$tau[[predictor]])
+  Tau <- diag(curr_tau)
   
   prop_log_prior <- mvtnorm::dmvnorm(prop_coef, mean = rep(0,nrow(Tau)), sigma = Tau, log = T)
   curr_log_prior <- mvtnorm::dmvnorm(curr_coef, mean = rep(0,nrow(Tau)), sigma = Tau, log = T)
@@ -75,24 +75,24 @@ mmala_update_spike <- function(curr_m, predictor, stepsize) {
 
 #' @importFrom stats fitted resid
 
-score_beta_spike <- function(m) {
-  drop((resid(m) / fitted(m, "scale")^2) %*% m$x - coef(m,'location')/m$tau$location)
+score_beta_spike <- function(m, curr_tau) {
+  drop((resid(m) / fitted(m, "scale")^2) %*% m$x - coef(m,'location')/curr_tau)
 }
 
 #' @importFrom stats resid
 
-score_gamma_spike <- function(m) {
-  drop((resid(m, "pearson")^2 - 1) %*% m$z - coef(m,'scale')/m$tau$scale)
+score_gamma_spike <- function(m, curr_tau) {
+  drop((resid(m, "pearson")^2 - 1) %*% m$z - coef(m,'scale')/curr_tau)
 }
 
 #' @importFrom stats fitted
 
-info_beta_spike <- function(m) {
-  crossprod(m$x / fitted(m, "scale")) + diag(c(1/m$tau$location))
+info_beta_spike <- function(m, curr_tau) {
+  crossprod(m$x / fitted(m, "scale")) + diag(c(1/curr_tau))
 }
 
-info_gamma_spike<- function(m) {
-  2 * crossprod(m$z) + diag(c(1/m$tau$scale))
+info_gamma_spike<- function(m, curr_tau) {
+  2 * crossprod(m$z) + diag(c(1/curr_tau))
 }
 
 # wrappers ------------------------------------------------------
@@ -102,15 +102,15 @@ score_funs_spike <- list(
   scale = score_gamma_spike
 )
 
-score_spike <- function(m, predictor) {
-  score_funs_spike[[predictor]](m)
+score_spike <- function(m, curr_tau, predictor) {
+  score_funs_spike[[predictor]](m, curr_tau)
 }
 
 chol_info_funs_spike <- list(
-  location = function(m) chol(info_beta_spike(m)),
-  scale = function(m) chol(info_gamma_spike(m))
+  location = function(m, curr_tau) chol(info_beta_spike(m, curr_tau)),
+  scale = function(m, curr_tau) chol(info_gamma_spike(m, curr_tau))
 )
 
-chol_info_spike <- function(m, predictor) {
-  chol_info_funs_spike[[predictor]](m)
+chol_info_spike <- function(m, curr_tau, predictor) {
+  chol_info_funs_spike[[predictor]](m, curr_tau)
 }
