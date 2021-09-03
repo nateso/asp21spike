a587aeec5ea2d24af5cc5dd30f416afdd4306129
Jan Schick <jan.schick@stud.uni-goettingen.de>
2021-09-03 17:09:39 +0200

    summary method updated

--- R/summary.R
+++ R/summary.R
@@ -9,22 +9,24 @@
 summary.lmls_spike <- function(object, ...) {
   x <- object
   coefs_loc <- x$spike$coefs$location
-  delta_loc <- x$spike$delta$location
-  coefs_loc <- coefs_loc[,names(coefs_loc) %in% names(delta_loc)]
-  probs_loc <- colMeans(delta_loc)
   means_loc <- colMeans(coefs_loc)
+  delta_loc <- x$spike$delta$location
+  probs_loc <- rep(NA, length(means_loc))
+  sel <- names(coefs_loc) %in% names(delta_loc)
+  probs_loc[sel] <- colMeans(delta_loc)
   sd_loc <- apply(coefs_loc, 2, sd)
   res_loc <- matrix(c(probs_loc, means_loc, sd_loc), 
                     ncol = 3, 
-                    dimnames = list(colnames(delta_loc),
+                    dimnames = list(colnames(coefs_loc),
                                     NULL))
   res_loc <- format(res_loc, digits = 3)
+  res_loc <- gsub("NA", " -", res_loc)
   res_loc <- rbind(c("Inc. prob.", "Posterior mean", "Posterior sd"),
                    res_loc)
   res_loc <- format(res_loc, justify = "right")
   res_loc <- cbind(res_loc,
                    c("   ",
-                     ifelse(probs_loc > 0.5,
+                     ifelse(!is.na(probs_loc) & probs_loc > 0.5,
                             ifelse(probs_loc > 0.75,
                                    ifelse(probs_loc > 0.9,
                                           "***",
@@ -36,22 +38,24 @@ summary.lmls_spike <- function(object, ...) {
   
   # Scale
   coefs_scl <- x$spike$coefs$scale
-  delta_scl <- x$spike$delta$scale
-  coefs_scl <- coefs_scl[names(coefs_scl) %in% names(delta_scl)]
-  probs_scl <- colMeans(delta_scl)
   means_scl <- colMeans(coefs_scl)
+  delta_scl <- x$spike$delta$scale
+  probs_scl <- rep(NA, length(means_scl))
+  sel <- names(coefs_scl) %in% names(delta_scl)
+  probs_scl[sel] <- colMeans(delta_scl)
   sd_scl <- apply(coefs_scl, 2, sd)
   res_scl <- matrix(c(probs_scl, means_scl, sd_scl), 
                     ncol = 3, 
-                    dimnames = list(colnames(delta_scl),
+                    dimnames = list(colnames(coefs_scl),
                                     NULL))
   res_scl <- format(res_scl, digits = 3)
+  res_scl <- gsub("NA", " -", res_scl)
   res_scl <- rbind(c("Inc. prob.", "Posterior mean", "Posterior sd"),
                    res_scl)
   res_scl <- format(res_scl, justify = "right")
   res_scl <- cbind(res_scl,
                    c("   ",
-                     ifelse(probs_scl > 0.5,
+                     ifelse(!is.na(probs_scl) & probs_scl > 0.5,
                             ifelse(probs_scl > 0.75,
                                    ifelse(probs_scl > 0.9,
                                           "***",
@@ -67,5 +71,6 @@ summary.lmls_spike <- function(object, ...) {
   prmatrix(res_loc, quote = FALSE)
   cat("\nScale:\n")
   prmatrix(res_scl, quote = FALSE)
-  cat("\n'***': Inc. prob. > 0.9 - '**': > 0.75 - '*': > 0.5\n")
+  cat("\nIncl. prob.:  > 0.9: '***'  |  > 0.75: '**'  |  > 0.5: '*'\n")
+  cat("Not selected: '-'")
 }
